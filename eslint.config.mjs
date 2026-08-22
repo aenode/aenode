95a5d5050d59a029495bb43f7f1dcece5b0f632a
AESYS | Ahmet Emrebas <aesys+git@proton.me>
2026-08-22 16:31:23 -0500

    dev: soome configuraiton

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,3 @@
 import base from './.eslint/configs/base.mjs';
 
-export default [
-  ...base,
-  {
-    files: ['**/*.json'],
-    // Override or add rules here
-    rules: {},
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser'),
-    },
-  },
-  {
-    ignores: ['**/vitest.config.*.timestamp*'],
-  },
-];
+export default [...base];
