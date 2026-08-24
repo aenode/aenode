82bab29f2802df62a123c959f8b5e56eb31882be
AESYS | Ahmet Emrebas <aesys+git@proton.me>
2026-08-24 15:36:16 -0500

    feat: created sample api application

---
+++ apps/api/src/index.ts
@@ -0,0 +1,2 @@
+// @index(['./app/**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}.js'`)
+export * from './app/app.module.js';
---
+++ apps/api/src/main.ts
@@ -0,0 +1,5 @@
+import { AppModule } from './app/app.module.js';
+
+export function main() {
+  console.log('Hello', AppModule.name);
+}
