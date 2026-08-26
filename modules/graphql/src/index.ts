36565a6a77515dbecc7cdacd5d73151c508f51ca
AESYS | Ahmet Emrebas <aesys+git@proton.me>
2026-08-25 21:12:43 -0500

    feat: centeralized the restapi/graphql componetns, decorators, and moudle form a single dedicated module

--- modules/graphql/src/index.ts
+++ modules/graphql/src/index.ts
@@ -1,6 +1,7 @@
 // @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}.js'`)
 export * from './app.module.js';
 export * from './common.module.js';
+export * from './decorators.js';
 export * from './global-validation-pipe.js';
 export * from './gql-cache.interceptor.js';
 export * from './prop.js';
---
+++ modules/prop/src/graphql/README.md
@@ -0,0 +1,1 @@
+# GraphQL Object/Input Decorators
