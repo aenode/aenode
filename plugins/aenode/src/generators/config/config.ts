b34333d5457fbfe2dfcc0ce84d88355525a11202
Ahmet Emrebas <aesys+github@proton.me>
2026-09-14 16:20:00 -0500

    fix: fixed config generator

--- plugins/aenode/src/generators/config/config.ts
+++ plugins/aenode/src/generators/config/config.ts
@@ -1,28 +1,24 @@
-import {
-  formatFiles,
-  generateFiles,
-  getProjects,
-  type Tree
-} from '@nx/devkit';
-import * as path from 'node:path';
+import { formatFiles, generateFiles, getProjects, type Tree } from '@nx/devkit';
+import { join } from 'node:path';
 
 /**
  * Generate swc, eslint, tsconfig, and vitest configurations for the project.
- * 
- * @param tree 
- * @param options 
+ *
+ * @param tree
+ * @param options
  */
-export async function configGenerator(
-  tree: Tree
-) {
-
+export async function configGenerator(tree: Tree) {
   const projects = getProjects(tree);
 
   for (const [name, project] of projects) {
-    generateFiles(tree, path.join(__dirname, 'files'), project.root, { projectName: name });
+    if (project.root === '' || project.root === '.' || !project.sourceRoot) {
+      continue;
+    }
 
+    generateFiles(tree, join(import.meta.dirname, 'files'), project.root, {
+      projectName: name,
+    });
   }
-
 
   await formatFiles(tree);
 }
